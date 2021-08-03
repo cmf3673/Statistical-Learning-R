2f31aa71ada5256b9e5e18ec9ed34f52d810206c
Caleb Ford <45129343+cmf3673@users.noreply.github.com>
2021-08-02 19:50:59 -0500

    added original logistic regression

--- classification.R
+++ classification.R
@@ -26,4 +26,64 @@ summary(glm.fits)
 coef(glm.fits)
 summary(glm.fits)$coef
 summary(glm.fits)$coef[,4] # Z scores
+# use predict() to get probability of going up (Y) based on all other data (X) aka P(Y=1|X) (use response to specify this is what you want)
 glm.probs = predict(glm.fits, type = 'response')
+glm.probs[1:10]
+# now convert predicted probabilities into "Up" or "Down" predictions
+glm.pred = rep('Down', 1250) # creates vector of 1250 "Down" elements
+glm.pred[glm.probs > .5] = 'Up' # changes to "Up" for all with a predicted probability > .5
+table(glm.pred, Direction) # create confusion matrix to see how many correctly predicted
+mean(glm.pred==Direction) # aka (# correctly predicted) / (total) = (507+145)/1250 = .5216
+# Logistic regression only predicted correct direction 52.16% of the time on TRAINING data, so likely overly optimistic
+# Now, hold out some data to get a realistic idea of how the model performs on unseen data
+# adding a column that says if year is < 2005, this is what we will train on 
+train = Year < 2005
+summary(train)
+length(train)
+Smarket.2005 = Smarket[!train,] # puts all 2005 rows in a matrix (a submatrix)
+Direction.2005 = Direction[!train]
+# Now fit logistical regression to all those observations not in 2005
+# subset=train only trains using data where train == True
+glm.fits = glm(Direction ~ Lag1 + Lag2 + Lag3 + Lag4 + Lag5 + Volume, data = Smarket, family = binomial, subset = train)
+glm.probs = predict(glm.fits, Smarket.2005, type = 'response')
+glm.pred = rep('Down', 252)
+glm.pred[glm.probs > .5] = 'Up'
+table(glm.pred, Direction.2005)
+mean(glm.pred == Direction.2005) # only 48% correct, worse than random guessing
+mean(glm.pred != Direction.2005) # 52% error rate
+
+# Logistic regression with nasa data to predict hazardous vs non-hazardous asteroids
+setwd('/Users/cf/Desktop/Statistical-Learning-R')
+asteroids.data = read.csv('asteroids_data.csv')
+asteroids.data = Filter(is.numeric, asteroids.data)
+# looking for non-corr variables 
+corr = cor(asteroids.data)
+pairs(asteroids.data[, 3:15])
+asteroids.data$Hazardous[asteroids.data$Hazardous == 'True'] = 1
+asteroids.data$Hazardous[asteroids.data$Hazardous == 'False'] = 0
+typeof(asteroids.data$Hazardous)
+hazardous = strtoi(asteroids.data$Hazardous)
+paste('percent hazardous in data:', sum(hazardous) / length(hazardous))
+str(asteroids.data)
+asteroids.data$Hazardous <- as.numeric(asteroids.data$Hazardous)
+asteroids.train = asteroids.data[1:3500,]
+asteroids.test = asteroids.data[3501:4687,]
+
+glm.fits = glm(Hazardous ~ Absolute.Magnitude + Est.Dia.in.M.max. + Relative.Velocity.km.per.sec + Orbit.Uncertainity + Jupiter.Tisserand.Invariant + Minimum.Orbit.Intersection + Eccentricity, data = asteroids.train, family = binomial)
+summary(glm.fits)
+
+glm.probs = predict(glm.fits, asteroids.test, type = 'response')
+glm.pred = rep(0, 1187)
+glm.pred[glm.probs > .5] = 1
+mean(glm.pred == asteroids.test$Hazardous) # 96.6% correct
+#cancer.data = cancer.data[,2:11]
+#cancer.data = cancer.data[cancer.data$type_of_breast_surgery == 'MASTECTOMY' | cancer.data$type_of_breast_surgery == 'BREAST CONSERVING',]
+#Mastectomy = rep(0, nrow(cancer.data))
+#Mastectomy[cancer.data$type_of_breast_surgery == 'MASTECTOMY'] = 1
+#cancer.data$mastectomy = Mastectomy
+#pairs(cancer.data)
+#glm(type_of_breast_surgery~., data = cancer.data, family = 'binomial')
+
+
+
+
